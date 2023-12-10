afc1a1a4bc70492b09f227a1cf97353ab0c9be8f
ireneaaa <148827894+ireneaaa@users.noreply.github.com>
2023-12-09 19:10:16 -0500

    update serial number

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 
 CREATE TABLE IF NOT EXISTS nyc_zip_codes (
-    id SERIAL PRIMARY KEY,
+    zip_id SERIAL PRIMARY KEY,
     boundary GEOMETRY(Polygon),
     zipcode VARCHAR(10)
 );
@@ -10,11 +10,13 @@ CREATE TABLE IF NOT EXISTS complaints_311 (
     complaint_id SERIAL PRIMARY KEY,
     geometry GEOMETRY(Point),
     zipcode VARCHAR(10),
-    created_at TIMESTAMP
+    created_at TIMESTAMP,
+    complaint_type VARCHAR(50)
 );
 
 
 CREATE TABLE IF NOT EXISTS trees (
+    tree_id SERIAL PRIMARY KEY,
     zipcode VARCHAR(10),
     created_at DATE,
     geometry GEOMETRY(Point),
